10db72af606e4730412db8f155d9720ae6f3e0a4
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-06-30 13:04:36 -0400

    First export to markdown working

--- FileInterpreter.py
+++ FileInterpreter.py
@@ -15,8 +15,9 @@ def get_all_forms():
 
 
 ## Helper function to determine the human readable skill scores for a participant.
-def extract_scores():
-    with open("/Volumes/RestifyVolume/recruitment/arman-izadi/self-assessment.txt") as f:
+## Argument is string pointing to the the form as absolute path
+def extract_scores(form):
+    with open(form) as f:
         lines = f.readlines()
         # print(lines)
 
@@ -41,4 +42,35 @@ def extract_scores():
         print(answers)
         return answers
 
-extract_scores()
+
+## argument is the form location, not the form content
+def build_participant_line(form):
+
+    line = "|"
+
+    ## extract participant name from form file location
+    participant_name = str(form).split("/")[4]
+    line = line + "*"+participant_name + "* |"
+
+    ## extract participant skills from form file
+    participant_skills = extract_scores(form)
+    for skill in participant_skills:
+        line = line + str(skill) + "|"
+
+    ## add total score
+    line = line + str(sum(participant_skills))+"|\n"
+    return line
+
+
+def build_markdown_grid():
+    markdown = "# Recruitment\nBelow scores are auto extraced from the self-assessment forms. Recruitment answers range from 1-5 where 5 indicates the highest experience.\n\n| **Name** | Java | Spring | MVN | T-CORE | UNIX | REST | Singl. | Refl. | *Total* |\n|---|---|---|---|---|---|---|---|---|---|\n"
+    text_file = open("/tmp/recruitment.md", 'w')
+    text_file.write(markdown)
+
+    ## Add an entry for every recruitment form detected
+    for form in get_all_forms():
+        text_file.write(build_participant_line(form))
+
+    text_file.close()
+
+build_markdown_grid()
