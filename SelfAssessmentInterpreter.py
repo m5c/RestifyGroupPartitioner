392b46ddde9e0149dd637fa851e8ccef9d3609c7
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-07-11 14:22:16 -0400

    preparing email generator

--- SelfAssessmentInterpreter.py
+++ SelfAssessmentInterpreter.py
@@ -1,11 +1,11 @@
-import FileInterpreter
+import SelfScoreFileParser
 import MarkDownPrinter
 from distributor.ScoreBasedDistributor import ScoreBasedDistributor
 from distributor.MinniMaxOptimizer import optimize_once, optimize
 
 # Main script logic
-FileInterpreter.verify_input_files()
-participants = FileInterpreter.extract_participants()
+SelfScoreFileParser.verify_input_files()
+participants = SelfScoreFileParser.extract_participants()
 
 # If you are not interested in building control groups and only want to see the overall stats, just comment out the
 # next line and change the MarkDownPrinter command to the one commented out.
